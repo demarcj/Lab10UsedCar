76d4e8fa913c9e291d7004db0e8db1a0e5c95205
Demarcj <demarcjohnson@yahoo.com>
2017-02-17 22:29:38 -0500

    First Commit

---
+++ src/bootcamp/UsedCar.java
@@ -0,0 +1,18 @@
+package bootcamp;
+
+/**
+ * Created by DeMarcj on 2/17/2017.
+ */
+public class UsedCar extends Car {
+
+    private double mileage;
+    
+
+    public UsedCar(String make, String model, int year, double price){
+        super(make, model, year, price);
+    }
+
+    public String toString() {
+        return super.toString();
+    }
+}
