ccac4e1a8907a3a775b1f701569998ed449df76d
DeMarc Johnson <demarcjohnson@yahoo.com>
2017-02-18 20:17:31 -0500

    Adding the polymorphism

--- src/bootcamp/CarApp.java
+++ src/bootcamp/CarApp.java
@@ -9,38 +9,32 @@ import java.util.Scanner;
 public class CarApp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        getCars(scan, "Welcome to the Grand Circus Motors admin console!");
+        ArrayList<Car> carList = getCars(scan, "Welcome to the DeMarc Johnson Motors console!");
+        choiceCars(carList);
         System.out.println("Thank you!");
         scan.close();
     }
 
-    public static void getCars(Scanner scan, String salutation){
+    public static ArrayList getCars(Scanner scan, String salutation){
         System.out.println(salutation);
-        System.out.println("How many cars are enter: ");
-        int input = scan.nextInt();
         ArrayList<Car> carList = new ArrayList<Car>();
-
-        for(int i = 0; i < input; i++){
-            System.out.println("Enter the car make: ");
-            String car = scan.next();
-            scan.nextLine();
-
-            System.out.println("Enter the car model: ");
-            String model = scan.nextLine();
-
-            System.out.println("Enter the car year: ");
-            int year = scan.nextInt();
-
-            System.out.println("Enter the car price: ");
-            double price = scan.nextDouble();
-
-            Car cars = new Car(car, model, year, price);
-
-            carList.add(cars);
-        }
-        for(Car cars: carList){
+        	Car car1 = new Car("Nikolai", "Model S", 2017, 54999.90);
+        	Car car2 = new Car("Ford", "Escapade", 2017, 54999.90);
+        	Car car3 = new Car("Chewie", "Vette", 2017, 54999.90);
+        	Car car4 = new UsedCar("Hyonda", "Prior", 2015, 54999.90, 35987.6);
+        	Car car5 = new UsedCar("Grand", "Chirpus", 2013, 54999.90, 12345.0);
+        	Car car6 = new UsedCar("Ponitac", "Witherell", 2016, 54999.90, 3500.3);
+            carList.add(car1);
+            return carList;
+    
+    }
+    
+    public static void choiceCars(ArrayList<Car> carList){
+    	System.out.println("Type in a number to pick a car.");
+    	for(Car cars: carList){
             System.out.println(cars);
             System.out.println("");
         }
+    	System.out.println("Which car would you like?");
     }
 }
--- src/bootcamp/UsedCar.java
+++ src/bootcamp/UsedCar.java
@@ -8,11 +8,12 @@ public class UsedCar extends Car {
     private double mileage;
     
 
-    public UsedCar(String make, String model, int year, double price){
+    public UsedCar(String make, String model, int year, double price, double mileage){
         super(make, model, year, price);
+        this.mileage = mileage;
     }
 
     public String toString() {
-        return super.toString();
+        return super.toString() + " (Used) " + mileage + " miles";
     }
 }
